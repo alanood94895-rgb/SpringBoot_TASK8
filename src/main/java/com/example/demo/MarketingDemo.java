047e431186d82d00f0af0411e7eb102937af5133
CompanyProfile <alanood94895@gmail.com>
2026-06-08 10:35:04 +0400

    Create new class name MarketingDemo

--- src/main/java/com/example/demo/MarketingDemo.java
+++ src/main/java/com/example/demo/MarketingDemo.java
@@ -1,4 +1,21 @@
 package com.example.demo;
 
+import com.example.demo.Entities.Campaign;
+import com.example.demo.service.CampaignManager;
+
 public class MarketingDemo {
+    public static void main(String[] args) {
+
+        CampaignManager manager = new CampaignManager();
+
+        System.out.println("Existing Campaigns");
+        manager.displayCampaigns();
+
+        Campaign newCampaign = new Campaign("C104", "Ramadan Offer", "Facebook Ads", 700);
+        System.out.println("\nPOST Operation");
+        System.out.println(manager.addCampaign(newCampaign));
+
+        System.out.println("\nUpdated Campaign List");
+        manager.displayCampaigns();
+    }
 }
