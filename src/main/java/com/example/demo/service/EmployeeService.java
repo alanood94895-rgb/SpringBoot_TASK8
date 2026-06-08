e7a0a42f675d3ad9d1c520a936b8d3f8a7d6bbbe
CompanyProfile <alanood94895@gmail.com>
2026-06-08 10:04:45 +0400

    display Employees

--- src/main/java/com/example/demo/service/EmployeeService.java
+++ src/main/java/com/example/demo/service/EmployeeService.java
@@ -1,4 +1,24 @@
 package com.example.demo.service;
 
+import com.example.demo.Entities.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class EmployeeService {
+
+    private List<Employee> employees = new ArrayList<>();
+
+    public EmployeeService() {
+        employees.add(new Employee("E101", "Sara", "HR"));
+        employees.add(new Employee("E102", "Ahmed", "Finance"));
+        employees.add(new Employee("E103", "John", "Sales"));
+    }
+
+    public void displayEmployees() {
+        for (Employee employee : employees) {
+            System.out.println(employee);
+        }
+    }
+
 }
