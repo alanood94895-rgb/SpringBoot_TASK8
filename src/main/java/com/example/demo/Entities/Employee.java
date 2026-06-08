3937d646e20f239a6237335330ab5b2aeac0a113
CompanyProfile <alanood94895@gmail.com>
2026-06-08 10:00:51 +0400

    Create Employee Class

--- src/main/java/com/example/demo/Entities/Employee.java
+++ src/main/java/com/example/demo/Entities/Employee.java
@@ -4,4 +4,27 @@ public class Employee {
     private String employeeId;
     private String employeeName;
     private String department;
+
+    public Employee(String employeeId, String employeeName, String department) {
+        this.employeeId = employeeId;
+        this.employeeName = employeeName;
+        this.department = department;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    @Override
+    public String toString() {
+        return employeeId + "  " + employeeName + "  " + department;
+    }
 }
