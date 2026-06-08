1bcfb4f490e296ff983579eb76c172cce41adcd8
CompanyProfile <alanood94895@gmail.com>
2026-06-08 10:18:57 +0400

    Create main class name CarRentalDemo

---
+++ src/main/java/com/example/demo/CarRentalDemo.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import com.example.demo.Entities.Vehicle;
+import com.example.demo.service.VehicleManager;
+
+public class CarRentalDemo {
+    public static void main(String[] args) {
+
+        VehicleManager manager = new VehicleManager();
+
+        System.out.println("Existing Vehicles");
+        manager.displayVehicles();
+
+        Vehicle newVehicle =
+                new Vehicle("V104", "Kia Sportage", 30);
+
+        System.out.println("\nPOST Operation");
+        System.out.println(manager.addVehicle(newVehicle));
+
+        System.out.println("\nUpdated Vehicle List");
+        manager.displayVehicles();
+    }
+}
