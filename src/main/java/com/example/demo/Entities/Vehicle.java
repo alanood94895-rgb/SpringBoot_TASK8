8199f990333542f9b8aa61cd6352c95f953ca9f6
CompanyProfile <alanood94895@gmail.com>
2026-06-08 10:16:10 +0400

    Create new class name Vehicle

--- src/main/java/com/example/demo/Entities/Vehicle.java
+++ src/main/java/com/example/demo/Entities/Vehicle.java
@@ -1,4 +1,31 @@
 package com.example.demo.Entities;
 
 public class Vehicle {
+    private String vehicleId;
+    private String vehicleModel;
+    private double rentalPricePerDay;
+
+    public Vehicle(String vehicleId, String vehicleModel, double rentalPricePerDay) {
+        this.vehicleId = vehicleId;
+        this.vehicleModel = vehicleModel;
+        this.rentalPricePerDay = rentalPricePerDay;
+    }
+
+    public String getVehicleId() {
+        return vehicleId;
+    }
+
+    public String getVehicleModel() {
+        return vehicleModel;
+    }
+
+    public double getRentalPricePerDay() {
+        return rentalPricePerDay;
+    }
+
+    @Override
+    public String toString() {
+        return vehicleId + "  " + vehicleModel + " " + rentalPricePerDay + " OMR/day";
+    }
+
 }
