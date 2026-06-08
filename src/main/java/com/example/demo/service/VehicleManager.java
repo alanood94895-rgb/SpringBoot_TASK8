3130749ce934497dac67f51e35a9f6bc863c2ba2
CompanyProfile <alanood94895@gmail.com>
2026-06-08 10:17:41 +0400

    Create new class name VehicleManager

--- src/main/java/com/example/demo/service/VehicleManager.java
+++ src/main/java/com/example/demo/service/VehicleManager.java
@@ -1,4 +1,39 @@
 package com.example.demo.service;
 
+import com.example.demo.Entities.Vehicle;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class VehicleManager {
+    private List<Vehicle> vehicles = new ArrayList<>();
+
+    public VehicleManager() {
+        vehicles.add(new Vehicle("V101", "Toyota Corolla", 20));
+        vehicles.add(new Vehicle("V102", "Nissan Sunny", 18));
+        vehicles.add(new Vehicle("V103", "Hyundai Elantra", 22));
+    }
+
+    public String addVehicle(Vehicle vehicle) {
+
+        for (Vehicle existingVehicle : vehicles) {
+            if (existingVehicle.getVehicleId().equals(vehicle.getVehicleId())) {
+                return "Vehicle ID already exists\nNo vehicle was added";
+            }
+        }
+
+        vehicles.add(vehicle);
+
+        return "Vehicle Added Successfully\n" +
+                "Vehicle ID: " + vehicle.getVehicleId() + "\n" +
+                "Vehicle Model: " + vehicle.getVehicleModel() + "\n" +
+                "Rental Price Per Day: " + vehicle.getRentalPricePerDay() + " OMR\n" +
+                "Status: Created";
+    }
+
+    public void displayVehicles() {
+        for (Vehicle vehicle : vehicles) {
+            System.out.println(vehicle);
+        }
+    }
 }
