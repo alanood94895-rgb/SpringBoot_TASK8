fd0ee4818d52466f4fa21b00468fae0500832a06
CompanyProfile <alanood94895@gmail.com>
2026-06-09 11:37:34 +0400

    Add interface repository

--- src/main/java/com/example/demo/Interfaces/EmployeeRepository.java
+++ src/main/java/com/example/demo/Interfaces/EmployeeRepository.java
@@ -1,4 +1,8 @@
 package com.example.demo.Interfaces;
 
-public interface EmployeeRepository {
+import com.example.demo.Entities.Campaign;
+import com.example.demo.Entities.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee,String> {
 }
