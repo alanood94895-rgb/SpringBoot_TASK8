4d62c03814fb10a7a01f22ca709bb514153f075e
CompanyProfile <alanood94895@gmail.com>
2026-06-09 11:36:39 +0400

    Add interface repository

--- src/main/java/com/example/demo/Interfaces/CampaignRepository.java
+++ src/main/java/com/example/demo/Interfaces/CampaignRepository.java
@@ -1,4 +1,7 @@
 package com.example.demo.Interfaces;
 
-public interface CampaignRepository {
+import com.example.demo.Entities.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CampaignRepository extends JpaRepository<campaign,String> {
 }
